a37d9c4cde32fc2b478996b0d4e6e834086f0221
horiba yusuke <horiba.yusuke20@gmail.com>
2023-03-04 00:01:57 +0000

    review edit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,6 +15,34 @@ class ReviewsController < ApplicationController
       render "mountains/show"
     end
   end
+  
+  
+  def show
+    @review = Review.find(params[:id])
+  end
+  
+  def edit
+    @review = Review.find(params[:id])
+    if @review.user != current_user
+      redirect_to reviews_path, alert: '不正なアクセスです。'
+    end
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to review_path(@review), notice: '更新に成功しました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_back(fallback_location: root_path)
+  end
+  
 
   private
   def review_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: "home#top"
   devise_for :users
   resource :user, except: [:new, :create, :destroy]
-  resources :mountains do
-    resources :reviews, only: [:index, :create]
+  resources :mountains, shallow: true do
+    resources :reviews
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
